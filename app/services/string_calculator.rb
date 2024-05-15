4499307078be7378ef408b07c40783585f827e42
Prabu <prabulead24@gmail.com>
2024-05-15 23:54:00 +0530

    unit test when input contains custom delimiter

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,7 +2,14 @@ class StringCalculator
   def self.add(params_str)
     return 0 if params_str.empty?
 
-    given_inputs = params_str.split(",").map(&:to_i)
+    if params_str.start_with?("//")
+      delimiter, params_str = params_str[2..].split("\n", 2)
+      delimiters = /#{delimiter}/
+    else
+      delimiters = /,|\n/
+    end
+
+    given_inputs = params_str.split(delimiters).map(&:to_i)
     negatives = given_inputs.select { |n| n < 0 }
     raise "negative numbers not allowed #{negatives.join(', ')}" unless negatives.empty?
 
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -27,5 +27,15 @@ RSpec.describe StringCalculator, type: :model do
     it "raises an exception for multiple negative numbers" do
       expect { StringCalculator.add("1,-2,-3") }.to raise_error("negative numbers not allowed -2, -3")
     end
+
+    it "when given the numbers separated by newlines returns the sum of numbers" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+
+    it "when given the numbers separated by custom delimiters returns the sum of numbers" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
   end
 end
