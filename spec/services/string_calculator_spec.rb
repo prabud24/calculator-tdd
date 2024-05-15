103fc8924d00a48f87305486fbad2b27998b4166
Prabu <prabulead24@gmail.com>
2024-05-15 23:17:14 +0530

    unit test sum of the numbers

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -6,13 +6,15 @@ RSpec.describe StringCalculator, type: :model do
       expect(StringCalculator.add("")).to eq(0)
     end
 
-
-    it "when given a single number return that same number." do
+    it "when given a single number return that same number" do
       expect(StringCalculator.add("3")).to eq(3)
       expect(StringCalculator.add("6")).to eq(6)
       expect(StringCalculator.add("9")).to eq(9)
     end
 
+     it "when given a two comma-separated numbers return the sum of two numbers" do
+      expect(StringCalculator.add("1,5")).to eq(6)
+    end
 
   end
 end
