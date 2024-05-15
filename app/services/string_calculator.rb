e7a684e4c9e87cd48e7d9430399006e218de7290
Prabu <prabulead24@gmail.com>
2024-05-16 00:33:50 +0530

    refactor readable by create a method and reduce line of code

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,11 +1,8 @@
 class StringCalculator
   def self.add(params_str)
     return 0 if params_str.empty?
-    delimiters = /,|\n/
-    if params_str.start_with?("//")
-      delimiter, params_str = params_str[2..].split("\n", 2)
-      delimiters = /#{delimiter}/
-    end
+
+    delimiters, params_str = custom_delimiter(params_str)
 
     given_inputs = params_str.split(delimiters).map(&:to_i)
     negatives = given_inputs.select { |n| n < 0 }
@@ -14,4 +11,14 @@ class StringCalculator
     result = given_inputs.sum
     return result
   end
+
+  def self.custom_delimiter(input_string)
+    delimiters = /,|\n/
+    if input_string.start_with?("//")
+      delimiter, input_string = input_string[2..].split("\n", 2)
+      delimiters = /#{delimiter}/
+    end
+    return [delimiters, input_string]
+  end
+
 end
