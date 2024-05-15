ddf673af660f9dc94ca867762e5425f1cf4c459a
Prabu <prabulead24@gmail.com>
2024-05-15 23:12:21 +0530

    unit test when give single number return that same number.

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,5 +1,9 @@
 class StringCalculator
   def self.add(params_str)
-     return 0 if params_str.empty?
+    return 0 if params_str.empty?
+
+    given_inputs = params_str.split(",").map(&:to_i)
+    result = given_inputs.sum
+    return result
   end
 end
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 RSpec.describe StringCalculator, type: :model do
   describe ".add" do
-    it "returns 0 for an empty string" do
+    it "when an empty string return 0" do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+
+    it "when given a single number return that same number." do
+      expect(StringCalculator.add("3")).to eq(3)
+      expect(StringCalculator.add("6")).to eq(6)
+      expect(StringCalculator.add("9")).to eq(9)
+    end
+
+
   end
 end
