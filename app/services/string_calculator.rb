673ca481d0a621d596ac2fb48fb497716e4ed871
Prabu <prabulead24@gmail.com>
2024-05-15 23:32:35 +0530

    unit test throw an exception when input contains negative number

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -3,6 +3,9 @@ class StringCalculator
     return 0 if params_str.empty?
 
     given_inputs = params_str.split(",").map(&:to_i)
+    negatives = given_inputs.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(', ')}" unless negatives.empty?
+
     result = given_inputs.sum
     return result
   end
